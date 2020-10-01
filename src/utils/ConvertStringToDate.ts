63da4c04674080ef4f18c01bc8e60b24d23869f4
hugo miranda <hugo.miranda.souza@gmail.com>
2020-10-01 20:14:31 -0300

    fix: date format util

--- src/utils/ConvertStringToDate.ts
+++ src/utils/ConvertStringToDate.ts
@@ -1,9 +1,11 @@
 const convertStringToDate = (date: string): string => {
   const [day, month, year] = date.split('/');
 
-  const newDate = new Date(`${year}-${month}-${day}`).toISOString();
+  const newDate = new Date(`${year}-${month}-${day}`).setHours(3);
 
-  return newDate;
+  const dateToString = new Date(newDate).toLocaleString('en');
+
+  return dateToString;
 };
 
 export default convertStringToDate;
